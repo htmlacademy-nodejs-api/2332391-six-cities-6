c52c2fe831bdd1871b5155bfc3c029987e8de329
unknown <denis.e.khmyrov@gmail.com>
2024-01-28 13:58:59 +0300

    feat: async read method updated

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,13 +1,36 @@
 import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from "node:fs";
+
+const CHUNK_SIZE = 16384;
 
 export class TSVFileReader extends EventEmitter implements FileReader {
   constructor(private readonly filename: string) {
     super();
   }
 
-  public read(): void {
-    // TODO:
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let currentData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      currentData += chunk.toString();
+
+      while ((nextLinePosition = currentData.indexOf("\n")) >= 0) {
+        const completeRow = currentData.slice(0, nextLinePosition + 1);
+        currentData = currentData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
