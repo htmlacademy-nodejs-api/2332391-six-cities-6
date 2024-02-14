f76f57d6e127e2614a99ffd3e71b5ab2398b895c
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2024-02-14 22:00:48 +0300

    feat: category model and entity created

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,4 @@ export * from './offer.type.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
 export * from './user.type.js';
+export * from './category.type.js';
