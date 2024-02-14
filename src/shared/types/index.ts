d07a032fa12d661c9694efde9529e7bda7d01a61
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2024-02-14 22:12:45 +0300

    offer entity added

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,4 +3,5 @@ export * from './mock-server-data.type.js';
 export * from './component.enum.js';
 export * from './user.type.js';
 export * from './category.type.js';
-
+export * from './offer.type.js';
+export * from './offer-type.enum.js';
