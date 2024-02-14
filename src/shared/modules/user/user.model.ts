986e03720115666bd23ba867c57856443e78ba40
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2024-02-14 20:21:29 +0300

    feat: match and required fields added

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -4,15 +4,25 @@ import { User } from '../../types/index.js';
 export interface UserDocument extends User, Document {
   createdAt: Date,
   updatedAt: Date,
-}
+  }
 
 const userSchema = new Schema({
   email: {
     type: String,
     unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
   },
-  avatarPath: String,
-  firstname: String,
+  avatarPath: {
+    type: String,
+    required: true,
+    minlength: [5, 'Min length for avatar path is 5'],
+  },
+  firstname: {
+    type: String,
+    required: true,
+    minlength: [2, 'Min length for firstname is 2']
+  },
   lastname: String,
 }, { timestamps: true });
 
