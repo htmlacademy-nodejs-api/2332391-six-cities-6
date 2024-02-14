796a77ae429476744c1ee7a5440b3daa3f21be0c
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2024-02-14 20:37:12 +0300

    feat: new user entity description created

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,8 +1,18 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: false, default: '' })
   public avatarPath: string;
+
+  @prop({ required: true })
   public firstName: string;
+
+  @prop({ required: true })
   public lastName: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
